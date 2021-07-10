bc59f28c9bb433f2b00bd7da262e49ae0f161942
Edward <edward.xyz@qq.com>
2021-07-10 21:28:31 +0800

    Update Baidu净化.user.js

--- Baidu净化.user.js
+++ Baidu净化.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Baidu净化
-// @icon         https://baidu.com/favicon.ico
+// @icon         http://baidu.com/favicon.ico
 // @namespace    https://github.com/iCrackit/Tampermonkey
 // @version      1.0
 // @description  去除百度搜索结果中的所有广告 自适应DarkMode
@@ -15,7 +15,7 @@
     'use strict';
     var tag = document.createElement('style');
     var light = '#content_right{display:none !important;} .c-container{width:980px !important;} .wrapper_new .s_ipt_wr{border: 2px solid #4e71f2 !important;}';
-    var dark = 'body{background-color:#1a1a1a !important;} #content_right{display:none !important;} .c-container{width:980px !important;background-color: #1a1a1a !important;} .head_wrapper{background:#1a1a1a !important;} #kw{color:#fff !important;} .wrapper_new .s_ipt_wr{border: 2px solid #4e71f2 !important;} p{color:#888;} .pc-tabs-content_2Ga6e,a{color: #ccc !important;background: #1a1a1a !important;text-decoration:none !important;} a:hover{color:#315efb !important;} .wrapper_new #s_tab .cur-tab{color:#ccc !important;} #page{background-color: #1a1a1a !important;} .wrapper_new #foot{background-color: #1a1a1a;} #help{background: #1a1a1a !important;} #rs{background: #1a1a1a;} em{color:#ccc !important;text-decoration: none !important;} .new-pmd .c-abstract{color:#888;} .new-pmd .c-color-text{color:#868686} .op-video-vast-ul li.op-video-vast-smallicon-li a span i{color:#616161 !important;} .op-video-vast-border{border-top: 1px solid #00000000 !important;} .op-video-vast-ul li.op-video-vast-smallicon-li{border-bottom: 1px solid #00000000 !important;} .c-border{border: 1px solid #fff0 !important;} #wrapper.wrapper_new .bdpfmenu, #wrapper.wrapper_new .usermenu{background: #1a1a1a !important;} .new-pmd .recommend-none-border{border-color: #1a1a1a !important;} .c-tabs-nav{border-bottom: 1px solid #1a1a1a !important;background-color: #1a1a1a !important;} .c-tabs-nav .c-tabs-nav-selected{border-color: #fff0 #fff0 #fff0 #fff0 !important;background-color: #1a1a1a !important;color: #ccc !important;} textarea,span{color:#ccc;} ';
+    var dark = 'body{background-color:#1a1a1a !important;} #content_right{display:none !important;} .c-container{width:980px !important;background-color: #1a1a1a !important;} .head_wrapper{background:#1a1a1a !important;} #kw{color:#fff !important;} .wrapper_new .s_ipt_wr{background:#1a1a1a !important;border: 2px solid #4e71f2 !important;} p{color:#888;} .pc-tabs-content_2Ga6e,a{color: #ccc !important;background: #1a1a1a !important;text-decoration:none !important;} a:hover{color:#315efb !important;} .wrapper_new #s_tab .cur-tab{color:#ccc !important;} #page{background-color: #1a1a1a !important;} .wrapper_new #foot{background-color: #1a1a1a;} #help{background: #1a1a1a !important;} #rs{background: #1a1a1a;} em{color:#ccc !important;text-decoration: none !important;} .new-pmd .c-abstract{color:#888;} .new-pmd .c-color-text{color:#868686} .op-video-vast-ul li.op-video-vast-smallicon-li a span i{color:#616161 !important;} .op-video-vast-border{border-top: 1px solid #00000000 !important;} .op-video-vast-ul li.op-video-vast-smallicon-li{border-bottom: 1px solid #00000000 !important;} .c-border{border: 1px solid #fff0 !important;} #wrapper.wrapper_new .bdpfmenu, #wrapper.wrapper_new .usermenu{background: #1a1a1a !important;} .new-pmd .recommend-none-border{border-color: #1a1a1a !important;} .c-tabs-nav{border-bottom: 1px solid #1a1a1a !important;background-color: #1a1a1a !important;} .c-tabs-nav .c-tabs-nav-selected{border-color: #fff0 #fff0 #fff0 #fff0 !important;background-color: #1a1a1a !important;color: #ccc !important;} textarea,span{color:#ccc;} ';
     let media = window.matchMedia('(prefers-color-scheme: dark)');
 
     if (media.matches){
@@ -47,7 +47,7 @@
         unsafeWindow.$(document).ajaxSuccess(function(e, xhr, opt) { // Microsoft Edge 和 Safari 不支持 $(document).ajaxSuccess()
             document.head.appendChild(tag);
             $('#content_left>div').has('span:contains("广告")').remove();
-            setTimeout(function () { $('.c-container').has('.f13>span:contains("广告")').remove(); }, 2100); // 去除顽固性的延迟加载广告，一般延迟2秒左右。例如搜索“淘宝”，当页面加载完毕之后在搜索结果最前或最后会再插入一个广告。
+            setTimeout(function () { $('.c-container').has('.f13>a:contains("广告")').remove(); }, 2100); // 去除顽固性的延迟加载广告，一般延迟2秒左右。例如搜索“淘宝”，当页面加载完毕之后在搜索结果最前或最后会再插入一个广告。
             try{
                 $(".product-item.c-span6.first-line-product").attr("class", "product-item c-span6"); // 全网热卖商品展示区布局错误，例如搜索：iPhone
                 if(document.getElementsByClassName("op-img-portrait-con").length){
